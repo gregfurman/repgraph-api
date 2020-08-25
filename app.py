659258a90a72db96ac181b9e93b7fa7d3939d5f2
gregfurman <gregfurman99@gmail.com>
2020-08-25 15:34:09 +0200

    Created initial API

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource, reqparse
+import json
+
+def create_app(app_name='GRAPH_API'):
+   app = Flask(app_name)
+   return app
+
+app = create_app()
+api = Api(app)
+graphs = {}
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('graphs')
+
+class GraphAPI(Resource):
+   def get(self,graph_id):
+      return {graphs: graphs[graph_id]}
+
+   def post(self):
+      args = parser.parse_args()
+      graph_list = [json.loads(graph) for graph in args['graphs'].split("\n")]
+      
+      for graph in graph_list:
+         graphs[graph['id']] = graph
+
+      return {"graphs":len(graph_list)}
+      
+
+api.add_resource(GraphAPI, '/', endpoint="graphs")
+
+
+if __name__ == "__main__":
+   app.run(debug=True,threaded=True)
