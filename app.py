a003713e58584685abac3f1d2c8ed09170fda6f6
frmgre001 <gregfurman99@gmail.com>
2020-08-30 22:03:16 +0200

    Added some functionality for the GraphManipulator class to allow users to check for graph properties using a graph id. Fixed some bugs with the Graph module. Worked on the API.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse
 import json
-from graphs import Graph
+from graphs import GraphManipulator
 
 # def create_app(app_name='GRAPH_API'):
 #    app = Flask(app_name)
@@ -9,32 +9,78 @@ from graphs import Graph
 
 app = Flask(__name__)
 api = Api(app)
-graphs = {}
+graphs = GraphManipulator()
+
+class LoadGraphs(Resource):
+   def __init__(self):
+      self.reqparse = reqparse.RequestParser()
+      self.reqparse.add_argument('graphs')
+      super(LoadGraphs, self).__init__()
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('graphs')
+   def get(self):
+      args = self.reqparse.parse_args()
 
-class GraphAPI(Resource):
-   def get(self,graph_id):
-      return {graphs: graphs[graph_id]}
+      graph_ids = json.loads(args['graphs'])
+
+      if "graph_id" in graph_ids:
+         graph_id = graph_ids["graph_id"]
+      else:
+         graph_id = 0
+         
+      return {"graph": str(graphs.getGraph(graph_id))}
 
    def post(self):
-      args = parser.parse_args()
-      graph_list = [json.loads(graph) for graph in args['graphs'].split("\n")]
-      
+      args = self.reqparse.parse_args()
+      graph_list = json.loads("[" + args['graphs'].replace("}{", "},{")  + "]")
+
       for graph in graph_list:
-         graphs[graph['id']] = Graph(graph)
+         graphs.addGraph(graph)
+         
+         # graphs[graph['id']].display()
+         print(graph['source'])
+         print("Connected:",graphs.is_connected(graph['id']))
+         print("Acyclic:",graphs.is_cyclic(graph['id']))
+         print(graphs.longest_path(graph['id']))
+         print("************************")
+
+
 
          #########
 #        If you would like to test out graph methods you can do the following:
 #        graphs[graph['id']].function(parameters)
          #########
-
       return {"graphs":len(graph_list)}
       
+# class GetGraph(Resource):
+#    def __init__(self):
+#       self.reqparse = reqparse.RequestParser()
+#       self.reqparse.add_argument('check_graphs')
+#       super(CheckGraphsAPI,self).__init__()
 
-api.add_resource(GraphAPI, '/', endpoint="graphs")
+#    def get(self,graph_id):
+#        return str(graphs.getGraph(graph_id))
+
+
+class checkGraphs(Resource):
+   def __init__(self):
+      self.reqparse = reqparse.RequestParser()
+      self.reqparse.add_argument('args')
+      super(checkGraphs,self).__init__()
+
+   def get(self):
+      args = self.reqparse.parse_args()
+      json_args = json.loads(args['args'])
+
+      if set(["graph_id","node_id"]) == json_args.keys():
+         return graphs.getNodeNeighbours(json_args["graph_id"],json_args["node_id"])
+      elif set(["node_labels"]) == json_args.keys():
+         return graphs.getGraphs(json_args["node_labels"])
+
+
+api.add_resource(LoadGraphs, '/',endpoint="load")
+api.add_resource(checkGraphs, '/nodes',endpoint="nodes")
+
 
 
 if __name__ == "__main__":
--- graphs.py
+++ graphs.py
@@ -1,18 +1,21 @@
-from collections import Counter
-
 class Node:
+
+   """
+   The Node class holds a node's id, label, a list of incoming and outgoing Edge objects and a list of Token objects with which it is anchored to. 
+   """
+
    def __init__(self,node_input,tokens):
       self.id = node_input['id']
       self.label = node_input['label']
       self.incomingEdges = []
       self.outgoingEdges = []
-      self.anchors = {token_id: tokens[token_id] for token_id in range(node_input['anchors'][0]["from"],node_input['anchors'][0]["end"])}
+      self.anchors = {token_id: tokens[token_id] for token_id in range(node_input['anchors'][0]["from"],node_input['anchors'][0]["end"]+1)}
 
    def __eq__(self, other):
-      return other.id == self.id
+      return other.label == self.label
 
    def __ne__(self, other):
-      return not(other.id == self.id)
+      return not(other.label == self.label)
 
    def __str__(self):
       return f"ID: {self.id}\nLabel: {self.label}\nIncoming edges: {self.incomingEdges}\nOutgoing Edges: {self.outgoingEdges}\nAnchored: {list(self.anchors.values())}"
@@ -31,16 +34,30 @@ class Node:
    def compare_labels(self,other):
       return other.label == self.label
 
+   def get_neighbours(self,incoming=True,as_json=False):
+      if as_json:
+         return {"incoming":[edge.get_src().id for edge in self.incomingEdges], "outgoing" :[edge.get_trg().id for edge in self.outgoingEdges]}
+
+      if incoming:
+         return [edge.get_src().id for edge in self.incomingEdges]
+
+      return [edge.get_trg().id for edge in self.outgoingEdges]      
+
    def add_edge(self,edge):
-      if (self == edge.node_source):
+      if (self.id == edge.node_source.id):
          self.outgoingEdges.append(edge)
-      elif(self == edge.node_target):
+      elif(self.id == edge.node_target.id):
          self.incomingEdges.append(edge)
       else:
          raise Exception("Error adding edges") #error message or try catch
       
 
 class Edge:
+
+   """
+   The Edge class is a directional connection between 2 nodes, having a node source (node_src) and node target (node target). 
+   Each Edge also has a label and post-label.
+   """
    def __init__(self,node_src,node_trg,label,post_label):
       self.node_source = node_src
       self.node_target = node_trg
@@ -68,6 +85,9 @@ class Edge:
 
 
 class Token:
+   """
+   A Token object has an index, form, lemma and an optional carg.
+   """
    def __init__(self,token_input):
       self.index = token_input['index']
       self.form = token_input['form']
@@ -83,6 +103,10 @@ class Token:
       return f"[Form: {self.form}]"
 
 class Graph:
+   """
+   A Graph object keeps a list of all the Nodes, Edges, Tokens, the original sentence being parsed and a single variable for a top node.
+   There are multiple functions within the Graph class that allow for checking formal graph properties and longest directional & non-directional paths. 
+   """
    def __init__(self,graph_input):
       self.sentence = graph_input['input']
 
@@ -97,100 +121,123 @@ class Graph:
    def display(self):
       [print(node) for node in self.nodes.values()]
 
-   def as_adjacency_list(self,directed=True,as_object=False):
-      
-      if directed:
-         if (as_object):
-            return {node.id: [edge.get_trg() for edge in node.outgoingEdges] for node in self.nodes.values()}
+   def getNode(self,node_id):
+      return self.nodes[node_id]
 
-         return {node.id: [edge.get_trg().id for edge in node.outgoingEdges] for node in self.nodes.values()}
+   def getNodes(self):
+      return list(self.nodes.values)
 
+   def has_labels(self,labels):
+      return set(labels).issubset(set([node.label for node in self.nodes.values()]))
+   
+   def __str__(self):
+       return " ".join([str(node) for node in self.nodes.values()])
+
+
+   def BFS_cycle(self,transpose=False):
+
+     
+      if transpose:
+         nodes = {node.id: len(node.outgoingEdges) for node in self.nodes.values()}
       else:
-         
-         adj_list = {node.id: [edge.get_src().id for edge in node.incomingEdges] for node in self.nodes.values()}
-         adj_list_T = {node.id: [edge.get_trg().id for edge in node.outgoingEdges] for node in self.nodes.values()}
-
-         for key in adj_list.keys():
-            adj_list[key].extend(adj_list_T[key])
-
-      return adj_list
+         nodes = {node.id: len(node.incomingEdges) for node in self.nodes.values()}
+      queue = [key for key in nodes.keys() if nodes[key] ==0]
 
 
-   def BFS(self, s, graph):    
+      visited = 0
 
-      visited = [False] * (len(graph)) 
-      queue = [] 
-      queue.extend(graph[s]) 
-      visited[s] = True
+      while queue:
 
-      while queue: 
+         s = queue.pop(0)
 
-         s = queue.pop(0) 
+         visited += 1
 
-         for i in graph[s]: 
-               if visited[i] == False: 
-                  queue.append(i) 
-                  visited[i] = True
+         for node_id in self.nodes[s].get_neighbours(incoming=transpose):
+            nodes[node_id] -= 1
+            if (nodes[node_id] == 0):
+               queue.append(node_id)
 
-      return visited.count(True) == len(visited)
+      
+      return len(nodes.keys()) == visited
+
+   def BFS_connected(self,s_node):
+      visited = [False]*len(self.nodes)
+      queue = [s_node]
+
+      while queue:
+
+         s = queue.pop(0)
+
+         for node_id in self.nodes[s].get_neighbours(incoming=False) + self.nodes[s].get_neighbours(incoming=True):
+            if visited[node_id] == False:
+               queue.append(node_id)
+               visited[node_id]=True
+            
+      return visited.count(visited[0]) == len(visited)
+
+
 
    def is_connected(self):
 
-      adj_list = self.as_adjacency_list()
-      adj_list_T = self.as_adjacency_list(transpose=True)
-
-      for key in adj_list.keys():
-         if not(self.BFS(key,adj_list) and self.BFS(key,adj_list_T)):
+      for key in self.nodes.keys():
+         if not(self.BFS_connected(s_node=key)):
             return False
+            
+      return True
+
+   def is_cyclic(self):
+
+      if not(self.BFS_cycle()):
+         return False
 
       return True
 
 
-   def DFS(self,G,v,seen=None,path=None):
-      if seen is None: seen = []
+   def DFS(self,v,directed=False,visited=None,path=None):
+      if visited is None: visited = []
       if path is None: path = [v]
       
-      seen.append(v)
+      visited.append(v)
    
       paths = []
-      for t in G[v]:
-         if t not in seen:
+
+      if directed:
+         neighbours = self.nodes[v].get_neighbours(incoming=not(directed))
+      else:
+         neighbours = self.nodes[v].get_neighbours(incoming=directed) + self.nodes[v].get_neighbours(incoming=not(directed))
+
+      for t in neighbours:
+         if t not in visited:
                t_path = path + [t]
                paths.append(tuple(t_path))
-               paths.extend(self.DFS(G, t, seen[:], t_path))
+               paths.extend(self.DFS(t,directed, visited[:], t_path))
          
       return paths
+
 
    def findLongestPath(self,directed=True):
 
       paths = self.findAllPaths(directed=directed)
-      print(paths)
       max_len   = max(len(p) for p in paths)
       max_paths = [p for p in paths if len(p) == max_len]
 
       return {"max_paths":max_paths,"length":max_len}
 
-   def findAllPaths(self,directed=True,as_object=False):
+   def findAllPaths(self,directed=True):
 
       paths = []
 
       if (directed):
          for i in range(len(self.nodes)):
-            paths.extend(self.DFS(self.as_adjacency_list(directed=directed,as_object=as_object),i))
+            paths.extend(self.DFS(i,directed=True))
+
       else:
-         paths= self.DFS(self.as_adjacency_list(directed=directed,as_object=as_object),0)
+         paths= self.DFS(0)
+
       return paths
 
 
-
-
-   def subset(self):
-      pass
-
    def subgraph_search(self,subgraph):
-
-      # subgraph_all_paths = set([tuple(range(0,len(path))) for path in graph.findAllPaths()][5:9])
-      # graph_all_paths = set([tuple(range(0,len(path))) for path in self.findAllPaths()])
 
       edges = []
       for edge in self.edges:
@@ -199,29 +246,45 @@ class Graph:
                edges.append(edge)
 
 
+      if len(edges) > 1:
+         return True, edges
+
+      return False
+
    def adj_nodes(self,node_id):
 
       node = self.nodes[node_id]
-      return [edges.get_nodes() for edges in node.incomingEdges + node.outgoingEdges]
+      return node.get_neighbours(True) + node.get_neighbours(False) #remove node_id from get_nodes
 
+class GraphManipulator:
+   """
+   The GraphManipulator class serves as a controller for the all graphs loaded via the API.
+   """
+   def __init__(self):
+      self.Graphs = {}
 
+   def addGraph(self,graph_input):
+      self.Graphs[graph_input['id']] = Graph(graph_input)
+
+   def getGraph(self,graph_id):
+      return self.Graphs.get(graph_id,None)
+
+   def getNodeNeighbours(self,graph_id,node_id):
+      graph = self.getGraph(graph_id)
+
+      if graph is not None:
+         return graph.getNode(int(node_id)).get_neighbours(as_json=True)
       
+      return '{"error":"graph id does not exist"}'
 
-      # subgraph_all_paths = ([tuple(graph.nodes[node] for node in path) for path in graph.findAllPaths()][5:9])
-      # graph_all_paths = ([ tuple(self.nodes[node] for node in path) for path in self.findAllPaths()])
+   def getGraphs(self,node_labels):
+         return [graph for graph in self.Graphs.keys() if self.Graphs[graph].has_labels(node_labels)]
 
-      # subgraph_paths_len = len(subgraph_all_paths)
+   def is_cyclic(self, graph_id):
+      return self.Graphs[graph_id].is_cyclic()
 
-      # matching_paths = []
+   def longest_path(self, graph_id, directed=False):
+      return self.Graphs[graph_id].findLongestPath()
 
-      # for index in range(len(graph_all_paths)-subgraph_paths_len):
-
-      #    window = graph_all_paths[index:subgraph_paths_len]
-
-         # for path in window:
-            
-            # matching_paths.append(graph_all_paths[index:subgraph_paths_len])
-
-
-      
-
+   def is_connected(self,graph_id):
+      return self.Graphs[graph_id].is_connected()
